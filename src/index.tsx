b372348ad340a5a203844f7167ee4cce952176db
lilcatchya <ricardofranco2509@gmail.com>
2023-06-19 22:38:52 -0300

    pratos recomendados

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Inicio />
   </React.StrictMode>
 );
