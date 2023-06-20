74296514cf8644612da3e1b100a1cad64a2d3ce5
lilcatchya <ricardofranco2509@gmail.com>
2023-06-20 18:48:27 -0300

    reaproveitando com react routes

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -5,7 +5,6 @@ import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
-import Menu from 'components';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -13,7 +12,6 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
   return (
     <main>
-      <Menu />
       <header className={styles.header}>
         <div className={styles.header__text}>
                     A casa do código e da massa
