4a5ad13e089f754f035654ecc863882580e5c44b
lilcatchya <ricardofranco2509@gmail.com>
2023-06-19 21:31:42 -0300

    atualizando os tipos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
-import logo from 'assets/logo.svg'
 import styles from './Item.module.scss'
 import cardapio from '../itens.json'
+import classNames from 'classnames'
 
 type Props = typeof cardapio[0]
 
@@ -17,7 +17,10 @@ export default function Item(props: Props) {
                     <p> { description } </p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
+                    <div className={classNames({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+                    })}>
                         { category.label }
                     </div>
                     <div className={styles.item__porcao}>
